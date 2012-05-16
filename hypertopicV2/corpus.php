bdb3d1e48f357435fde87ebc735df9ba862abded
Aurelien Benel <aurelien.benel@utt.fr>
2012-05-16 22:08:06 +0200

    FEATURE: Hypertopic v2 corpus view (with real corpora).

--- hypertopicV2/corpus.php
+++ hypertopicV2/corpus.php
@@ -17,22 +17,28 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details:
 http://www.gnu.org/licenses/gpl.html
 */
 
-$dir = opendir('../picture');
-$db = new PDO('sqlite:../attribute/database');
-$result = $db->query('SELECT * FROM attributes');
-$row = array();
-foreach ($result as $row) {
-	$name[$row[0]] = $row[2];
-}
+include('../lib/Mustache.php');
 
-header('content-type: application/json;charset=utf-8');
-echo '{"rows":[', "\n", '{"key":["00"], "value":{"name":"Pictures"}}';
-while ($file=readdir($dir)) {
-	if (substr($file, 0, 1)!='.') {
-		echo ",\n", '{"key":["00","', $file, '"], "value":{"name":"', 
-			$name[$file], '"}}';
-	}
+$db = new PDO('sqlite:../attribute/database');
+$data = array(
+  'corpus' => $_GET['corpus'],
+  'service' => 'http://'.$_SERVER['HTTP_HOST'], //TODO non root or with port
+  'pictures' => array()
+);
+$query = $db->prepare(
+  "SELECT a1.source_id, a1.attribute_value FROM attributes a1, attributes a2 "
+  ."WHERE a1.source_id=a2.source_id AND a1.attribute_name='name' "
+  ."AND a2.attribute_name='corpus' AND a2.attribute_value=?"
+);
+$query->execute(array($_GET['corpus']));
+while ($row = $query->fetch()) {
+  $data['pictures'][] = array(
+    'item' => $row[0],
+    'name' => $row[1]
+  );
 }
-echo "\n]}";
+$renderer = new Mustache();
+header('content-type: application/json');
+echo $renderer->render(file_get_contents('../template/corpus.json'), $data);
 
 ?>
