f3ff4b81a9ab2468ff3730fc163e0b9ee85a99a0
Aurelien Benel <aurelien.benel@utt.fr>
2012-05-16 23:04:24 +0200

    FEATURE: Hypertopic v2 item view (with real corpora).

--- hypertopicV2/item.php
+++ hypertopicV2/item.php
@@ -17,35 +17,23 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details:
 http://www.gnu.org/licenses/gpl.html
 */
 
-function sendRow($item, $attribute, $value, &$isFirst) {
-	if ($isFirst) {
-		$isFirst = false;
-	} else {
-		echo ",\n";
-	}
-	echo '{"key":["00","', $item, '"], "value":{"', $attribute, '":"',
-		$value, '"}}';
-}
+include('../lib/Mustache.php');
 
-header('content-type: application/json;charset=utf-8');
-echo '{"rows":[', "\n";
-$id = $_GET['id'];
-if (file_exists("../picture/$id"))  {
-	$url_prefix = 'http://'.$_SERVER['HTTP_HOST'];//TODO https and non root
-
-	$db = new PDO('sqlite:../attribute/database');
-	$query = $db->prepare(
-		'SELECT * FROM attributes WHERE source_id=?'
-	);
-	$query->execute(array($id));
-
-	$isFirst = true;
-	foreach ($query->fetchAll() as $row) {
-		sendRow($id, $row[1], $row[2], $isFirst); 
-	}
-	sendRow($id, 'resource', $url_prefix.'/picture/'.$id, $isFirst);
-	sendRow($id, 'thumbnail', $url_prefix.'/thumbnail/'.$id, $isFirst);
-}
-echo "]}";
+$db = new PDO('sqlite:../attribute/database');
+$query = $db->prepare(
+  "SELECT attribute_value FROM attributes "
+  ."WHERE source_id=? AND attribute_name='name'"
+);
+$query->execute(array($_GET['item']));
+$result = $query->fetch();
+$data = array(
+  'corpus' => $_GET['corpus'],
+  'item' => $_GET['item'],
+  'service' => 'http://'.$_SERVER['HTTP_HOST'], //TODO non root or with port
+  'name' => $result[0]
+);
+$renderer = new Mustache();
+header('content-type: application/json');
+echo $renderer->render(file_get_contents('../template/item.json'), $data);
 
 ?>
