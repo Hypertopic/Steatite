4ee15d71b85a73c2b072f2f1271ea9e86f28c748
Aurelien Benel <aurelien.benel@utt.fr>
2012-05-17 18:55:36 +0200

    FEATURE: Hypertopic v2 user view (with real corpora but fake user).

--- hypertopicV2/user.php
+++ hypertopicV2/user.php
@@ -1,25 +1,40 @@
 <?php
 /*
-STEATITE - Web service for getting different views of archived documents
+STEATITE - Pictures archive for qualitative analysis
 
-Copyright (C) 2010 Aurelien Benel
+Copyright (C) 2010-2012 Aurelien Benel
 
 OFFICIAL WEB SITE
-http://www.hypertopic.org/index.php/Steatite
+http://www.hypertopic.org/
 
 LEGAL ISSUES
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License (version 2) as published by the
-Free Software Foundation.
+the terms of the GNU Affero General Public License as published by the Free 
+Software Foundation.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Public License for more details:
-http://www.gnu.org/licenses/gpl.html
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details:
+http://www.gnu.org/licenses/agpl.html
 */
 
-$id = $_GET['id'];
-header('content-type: application/json;charset=utf-8');
-echo '{"rows":[', "\n", '{"key":["', $id, 
-	'"], "value":{"corpus":{"id":"00", "name":"Pictures"}}}', "\n]}";
+include('../lib/Mustache.php');
+
+$db = new PDO('sqlite:../attribute/database');
+$data = array(
+  'user' => $_GET['id']
+);
+$result = $db->query(
+  "SELECT attribute_value, count(1) FROM attributes "
+  ."WHERE attribute_name='corpus' GROUP BY attribute_value"
+);
+foreach ($result as $row) {
+  $data['corpora'][] = array(
+    'id' => $row[0],
+    'count' => $row[1]
+  );
+}
+$renderer = new Mustache();
+header('content-type: application/json');
+echo $renderer->render(file_get_contents('../template/user.json'), $data);
 
 ?>
