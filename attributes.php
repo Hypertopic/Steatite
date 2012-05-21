7edad2e1fa77e22800c3dd5a8904e958be63a491
Aurelien Benel <aurelien.benel@utt.fr>
2012-05-21 16:51:40 +0200

    FEATURE: Delete a picture (i.e. remove all attributes but keep content).

--- attributes.php
+++ attributes.php
@@ -17,7 +17,9 @@ PARTICULAR PURPOSE. See the GNU Affero General Public License for more details:
 http://www.gnu.org/licenses/agpl.html
 */
 
-if ($_SERVER['REQUEST_METHOD']=='POST') {
+switch ($_SERVER['REQUEST_METHOD']) {
+  
+  case 'POST':
   $item = $_GET['source_id'];
   $attribute = $_GET['attribute_name'];
   $value = $_GET['attribute_value'];
@@ -31,7 +33,17 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
   );
   $deleteStatement->execute(array($item, $attribute));
   $insertStatement->execute(array($item, $attribute, $value));
-} else {
+  break;
+
+  case 'DELETE':
+  $db = new PDO('sqlite:attribute/database');
+  $deleteStatement =  $db->prepare(
+    'DELETE FROM attributes WHERE source_id=?'
+  );
+  $deleteStatement->execute(array($_GET['source_id']));
+  break;
+
+  default:
   header('Method not allowed', true, 405);
 }
 
