1a129514773042477dd6f34fcd16daeee8e6e0e6
Aurelien Benel <aurelien.benel@utt.fr>
2012-05-22 10:27:57 +0200

    FIX: Revision number.

--- controller/index.php
+++ controller/index.php
@@ -21,7 +21,7 @@ include('../lib/Mustache.php');
 
 if ('application/json'==$_SERVER['HTTP_ACCEPT']) {
   header('content-type: application/json');
-  echo('{"service":"Steatite", "revision":"6.2012.05.17"}');
+  echo('{"service":"Steatite", "revision":"6.2012.05.21"}');
 } else {
   $db = new PDO('sqlite:../attribute/database');
   $row = 
