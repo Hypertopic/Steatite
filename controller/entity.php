7257fb87a20a137b4fd892dce7bb49ad438960f9
Aurelien Benel <aurelien.benel@utt.fr>
2012-05-22 17:53:27 +0200

    FIX: Hypertopic v1 emulation is needed until Argos v1 data is migrated.

---
+++ controller/entity.php
@@ -0,0 +1,39 @@
+<?php
+/*
+STEATITE - Pictures archive for qualitative analysis
+
+Copyright (C) 2010-2012 Aurelien Benel
+
+OFFICIAL WEB SITE
+http://www.hypertopic.org/
+
+LEGAL ISSUES
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free 
+Software Foundation.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details:
+http://www.gnu.org/licenses/agpl.html
+*/
+
+include('../lib/Mustache.php');
+
+$db = new PDO('sqlite:../attribute/database');
+$query = $db->prepare(
+  "SELECT attribute_value FROM attributes "
+  ."WHERE source_id=? AND attribute_name='name'"
+);
+$query->execute(array($_GET['id']));
+$result = $query->fetch();
+preg_match('#(.+)/entity/#', $_SERVER['REQUEST_URI'], $path);
+$data = array(
+  'item' => $_GET['id'],
+  'service' => 'http://'.$_SERVER['HTTP_HOST'].$path[1], //TODO port
+  'name' => $result[0]
+);
+$renderer = new Mustache();
+header('content-type: text/xml');
+echo $renderer->render(file_get_contents('../view/entity.xml'), $data);
+
+?>
